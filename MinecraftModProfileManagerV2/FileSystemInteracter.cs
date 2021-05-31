8105ed20ffb467cce8d69f233fd14085e49424b2
TastyPie2 <74426910+TastyPie2@users.noreply.github.com>
2021-05-31 06:08:15 +0200

    Add project files.

---
+++ MinecraftModProfileManagerV2/FileSystemInteracter.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace MinecraftModProfileManagerV2
+{
+    public class FileSystemInteracter
+    {
+
+        static public readonly string ProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\Profiles";
+        static public readonly string ModsFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\Mods";
+        static public readonly string MinecraftInstallPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft";
+
+        static public async Task<List<Profile>> GetProfilesAsync()
+        {
+
+            await CheckForMinecraftInstallation();
+            await CheckNeededFolders();
+
+            List<Profile> profiles = new List<Profile>();
+            List<string> profilePaths = Directory.GetDirectories(ProfilePath).ToList();
+
+            List<Task> FileTrasfers = new List<Task>();
+
+            FileTrasfers.Add(Task.Run(() => profilePaths.ForEach(i => { Profile y = new Profile(); y.Path = i; y.Name = i.Split(@"\").Last(); profiles.Add(y); })));
+
+            FileTrasfers.ForEach(i => i.Wait());
+
+            return profiles;
+
+        }
+        
+        static public async Task AddProfileAsync(string name, MainWindow mainWindowInstance)
+        {
+            await CheckForMinecraftInstallation();
+            await CheckNeededFolders();
+
+            string newProfilePath = ProfilePath + @"\" + name;
+
+            /*if (Directory.Exists(newProfilePath))
+                throw new IOException();*/
+
+            Directory.CreateDirectory(newProfilePath);
+
+            Profile newProfile = new Profile() { Name = name, Path = newProfilePath};
+
+            mainWindowInstance.AddProfile(newProfile);
+
+        }
+
+        static public async Task UseChoosenProfile(Profile profile)
+        {
+            await CheckForMinecraftInstallation();
+            await CheckNeededFolders();
+
+            List<string> profileMods = Directory.GetFiles(profile.Path).ToList();
+
+            await ClearModsFolder();
+
+            List<Task> FileTrasfers = new List<Task>();
+
+            FileTrasfers.Add(Task.Run(() => profileMods.ForEach(i =>
+            {
+                string destPath = ModsFolder + @"\" + i.Split(@"\").Last();
+                File.Copy(i, destPath);
+            })));
+
+            FileTrasfers.ForEach(i => i.Wait());
+        }
+
+        static public async Task ClearModsFolder()
+        {
+            await CheckForMinecraftInstallation();
+            await CheckNeededFolders();
+
+            List<string> modsFolderContents = Directory.GetFiles(ModsFolder).ToList();
+            List<Task> FileTrasfers = new List<Task>();
+
+            FileTrasfers.Add(Task.Run(() => modsFolderContents.ForEach(i => File.Delete(i))));
+            FileTrasfers.ForEach(i => i.Wait());
+
+        }
+
+        public static async Task DeleteProfile(Profile profile)
+        {
+            await CheckForMinecraftInstallation();
+            await CheckNeededFolders();
+
+            List<string> folderContents = Directory.GetFiles(profile.Path).ToList();
+            List<Task> FileTrasfers = new List<Task>();
+
+            FileTrasfers.Add(Task.Run(() => folderContents.ForEach(i => File.Delete(i))));
+            FileTrasfers.ForEach(i => i.Wait());
+
+            Directory.Delete(profile.Path);
+
+        }
+
+        static public async Task AddFileToProfile(Profile profile, List<string> modPaths)
+        {
+            await CheckForMinecraftInstallation();
+            await CheckNeededFolders();
+
+            List<Task> FileTrasfers = new List<Task>();
+
+            modPaths.ForEach(i =>
+            {
+                FileTrasfers.Add(Task.Run(() => 
+                {
+                    string destPath = profile.Path + @"\" + i.Split(@"\").Last();
+                    File.Copy(i, destPath); 
+                }));
+            });
+
+            FileTrasfers.ForEach(i => i.Wait());
+
+        }
+
+        static public async Task CheckNeededFolders()
+        {
+            if (!Directory.Exists(ProfilePath))
+            {
+                Directory.CreateDirectory(ProfilePath);
+            }
+            if (!Directory.Exists(ModsFolder))
+            {
+                Directory.CreateDirectory(ModsFolder);
+            }
+        }
+
+        static public async Task CheckForMinecraftInstallation()
+        {
+            if(!Directory.Exists(MinecraftInstallPath))
+            {
+                MessageBox.Show("No minecraft installation was found","Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(-1);
+            }
+        }
+
+    }
+}
---
+++ MinecraftModProfileManagerV2/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MinecraftModProfileManagerV2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Profile> Profiles = new List<Profile>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async void LoadProfiles()
+        {
+            ProfilesSelector.Items.Clear();
+
+            List<Profile> ProfilesFromDir = FileSystemInteracter.GetProfilesAsync().GetAwaiter().GetResult();
+
+            ProfilesFromDir.ForEach(i => AddProfile(i));
+        }
+
+        public void AddProfile(Profile profile)
+        {
+            ProfilesSelector.Items.Add(string.Format("{0}", profile.Name));
+
+            profile.ListIndex = Profiles.Count;
+            Profiles.Add(profile);
+
+        }
+
+        private void PrimaryWindow__Init__(object sender, RoutedEventArgs e)
+        {
+            LoadProfiles();
+            ProfilesSelector.SelectedIndex = 0;
+
+            DeleteSelectedProfileButton.Margin = new Thickness(35,68,0,0);
+        }
+
+        private async void AddNewProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            await FileSystemInteracter.AddProfileAsync(ProfileNameTextBox.Text, this);
+            ProfileNameTextBox.Text = "";
+        }
+
+        private void UseSelectedProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileSystemInteracter.UseChoosenProfile(Profiles[ProfilesSelector.SelectedIndex]);
+        }
+
+        private void OpenInExplorerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Profile profile = Profiles[ProfilesSelector.SelectedIndex];
+
+            Process exporer = new Process();
+            exporer.StartInfo.FileName = "explorer.exe";
+            exporer.StartInfo.Arguments = profile.Path;
+
+            exporer.Start();
+        }
+
+        private async void DeleteSelectedProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult confirmationBoxResult = MessageBox.Show("This action cannot be undone, continue?", "Confirmation needed", MessageBoxButton.OKCancel, MessageBoxImage.Warning, MessageBoxResult.Cancel);
+
+            if(confirmationBoxResult == MessageBoxResult.OK)
+            {
+                await FileSystemInteracter.DeleteProfile(Profiles[ProfilesSelector.SelectedIndex]);
+                LoadProfiles();
+                ProfilesSelector.SelectedIndex = 0;
+            }
+
+
+        }
+    }
+}
